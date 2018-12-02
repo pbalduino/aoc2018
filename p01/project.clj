6db90785cff90a55c00fcca6f522d407f20a96a7
Plínio Balduino <pbalduino@users.noreply.github.com>
2018-12-02 11:22:30 +0100

    Remove unneeded dependency

--- p01/project.clj
+++ p01/project.clj
@@ -3,6 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [http-kit "2.2.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
   :main aoc2018.p01)
